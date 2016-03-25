2654acd43040f09e2f1aebe1cd7a5f1491ebf908
Andrea Falco <fox91fox@gmail.com>
2016-03-25 11:47:58 +0100

    Updated utility

--- build.sh
+++ build.sh
@@ -3,34 +3,44 @@ set -e
 set -u
 set -x
 
-docker pull fox91/nginx:latest
-
 # docker build -t fox91/nginx:latest ./
-docker tag -f fox91/nginx:latest docker.io/fox91/nginx:1.9.12
-docker tag -f fox91/nginx:latest docker.io/fox91/nginx:1.9
-docker tag -f fox91/nginx:latest docker.io/fox91/nginx:1
+docker pull fox91/nginx:latest
+IMAGE_ID=$(docker images -q fox91/nginx:latest)
+docker tag "${IMAGE_ID}" fox91/nginx:1.9.12
+docker tag "${IMAGE_ID}" fox91/nginx:1.9
+docker tag "${IMAGE_ID}" fox91/nginx:1
 
-docker build -t docker.io/fox91/nginx:profpm ./profpm
-docker tag -f fox91/nginx:profpm docker.io/fox91/nginx:1.9.12-profpm
-docker tag -f fox91/nginx:profpm docker.io/fox91/nginx:1.9-profpm
-docker tag -f fox91/nginx:profpm docker.io/fox91/nginx:1-profpm
+# docker build -t fox91/nginx:profpm ./profpm
+docker pull fox91/nginx:profpm
+IMAGE_ID=$(docker images -q fox91/nginx:profpm)
+docker tag "${IMAGE_ID}" fox91/nginx:1.9.12-profpm
+docker tag "${IMAGE_ID}" fox91/nginx:1.9-profpm
+docker tag "${IMAGE_ID}" fox91/nginx:1-profpm
 
-docker build -t docker.io/fox91/nginx:profpm-catchall ./profpm-catchall
-docker tag -f fox91/nginx:profpm-catchall docker.io/fox91/nginx:1.9.12-profpm-catchall
-docker tag -f fox91/nginx:profpm-catchall docker.io/fox91/nginx:1.9-profpm-catchall
-docker tag -f fox91/nginx:profpm-catchall docker.io/fox91/nginx:1-profpm-catchall
+# docker build -t fox91/nginx:profpm-catchall ./profpm-catchall
+docker pull fox91/nginx:profpm-catchall
+IMAGE_ID=$(docker images -q fox91/nginx:profpm-catchall)
+docker tag "${IMAGE_ID}" fox91/nginx:1.9.12-profpm-catchall
+docker tag "${IMAGE_ID}" fox91/nginx:1.9-profpm-catchall
+docker tag "${IMAGE_ID}" fox91/nginx:1-profpm-catchall
 
-docker build -t docker.io/fox91/nginx:ssl ./ssl
-docker tag -f fox91/nginx:ssl docker.io/fox91/nginx:1.9.12-ssl
-docker tag -f fox91/nginx:ssl docker.io/fox91/nginx:1.9-ssl
-docker tag -f fox91/nginx:ssl docker.io/fox91/nginx:1-ssl
+# docker build -t fox91/nginx:ssl ./ssl
+docker pull fox91/nginx:ssl
+IMAGE_ID=$(docker images -q fox91/nginx:ssl)
+docker tag "${IMAGE_ID}" fox91/nginx:1.9.12-ssl
+docker tag "${IMAGE_ID}" fox91/nginx:1.9-ssl
+docker tag "${IMAGE_ID}" fox91/nginx:1-ssl
 
-docker build -t docker.io/fox91/nginx:ssl-profpm ./ssl-profpm
-docker tag -f fox91/nginx:ssl-profpm docker.io/fox91/nginx:1.9.12-ssl-profpm
-docker tag -f fox91/nginx:ssl-profpm docker.io/fox91/nginx:1.9-ssl-profpm
-docker tag -f fox91/nginx:ssl-profpm docker.io/fox91/nginx:1-ssl-profpm
+# docker build -t fox91/nginx:ssl-profpm ./ssl-profpm
+docker pull fox91/nginx:ssl-profpm
+IMAGE_ID=$(docker images -q fox91/nginx:ssl-profpm)
+docker tag "${IMAGE_ID}" fox91/nginx:1.9.12-ssl-profpm
+docker tag "${IMAGE_ID}" fox91/nginx:1.9-ssl-profpm
+docker tag "${IMAGE_ID}" fox91/nginx:1-ssl-profpm
 
-docker build -t docker.io/fox91/nginx:ssl-profpm-catchall ./ssl-profpm-catchall
-docker tag -f fox91/nginx:ssl-profpm-catchall docker.io/fox91/nginx:1.9.12-ssl-profpm-catchall
-docker tag -f fox91/nginx:ssl-profpm-catchall docker.io/fox91/nginx:1.9-ssl-profpm-catchall
-docker tag -f fox91/nginx:ssl-profpm-catchall docker.io/fox91/nginx:1-ssl-profpm-catchall
+# docker build -t fox91/nginx:ssl-profpm-catchall ./ssl-profpm-catchall
+docker pull fox91/nginx:ssl-profpm-catchall
+IMAGE_ID=$(docker images -q fox91/nginx:ssl-profpm-catchall)
+docker tag "${IMAGE_ID}" fox91/nginx:1.9.12-ssl-profpm-catchall
+docker tag "${IMAGE_ID}" fox91/nginx:1.9-ssl-profpm-catchall
+docker tag "${IMAGE_ID}" fox91/nginx:1-ssl-profpm-catchall
