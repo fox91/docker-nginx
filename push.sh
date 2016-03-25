e4c91d56cb95ca6ffb4448caf8990000821180ac
Andrea Falco <fox91fox@gmail.com>
2016-03-25 10:51:55 +0100

    nginx 1.9.12

--- push.sh
+++ push.sh
@@ -3,31 +3,31 @@ set -e
 set -u
 set -x
 
-docker push docker.io/fox91/nginx:1.9.11
+docker push docker.io/fox91/nginx:1.9.12
 docker push docker.io/fox91/nginx:1.9
 docker push docker.io/fox91/nginx:1
 
 docker push docker.io/fox91/nginx:profpm
-docker push docker.io/fox91/nginx:1.9.11-profpm
+docker push docker.io/fox91/nginx:1.9.12-profpm
 docker push docker.io/fox91/nginx:1.9-profpm
 docker push docker.io/fox91/nginx:1-profpm
 
 docker push docker.io/fox91/nginx:profpm-catchall
-docker push docker.io/fox91/nginx:1.9.11-profpm-catchall
+docker push docker.io/fox91/nginx:1.9.12-profpm-catchall
 docker push docker.io/fox91/nginx:1.9-profpm-catchall
 docker push docker.io/fox91/nginx:1-profpm-catchall
 
 docker push docker.io/fox91/nginx:ssl
-docker push docker.io/fox91/nginx:1.9.11-ssl
+docker push docker.io/fox91/nginx:1.9.12-ssl
 docker push docker.io/fox91/nginx:1.9-ssl
 docker push docker.io/fox91/nginx:1-ssl
 
 docker push docker.io/fox91/nginx:ssl-profpm
-docker push docker.io/fox91/nginx:1.9.11-ssl-profpm
+docker push docker.io/fox91/nginx:1.9.12-ssl-profpm
 docker push docker.io/fox91/nginx:1.9-ssl-profpm
 docker push docker.io/fox91/nginx:1-ssl-profpm
 
 docker push docker.io/fox91/nginx:ssl-profpm-catchall
-docker push docker.io/fox91/nginx:1.9.11-ssl-profpm-catchall
+docker push docker.io/fox91/nginx:1.9.12-ssl-profpm-catchall
 docker push docker.io/fox91/nginx:1.9-ssl-profpm-catchall
 docker push docker.io/fox91/nginx:1-ssl-profpm-catchall
